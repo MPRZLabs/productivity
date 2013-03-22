99bbe98a51b4c5b63e621e637194827efe8b0c08
michcioperz <michcioperz@gmail.com>
2013-03-22 21:55:21 +0100

    Moving some code to make the design more clear

--- invent.php
+++ invent.php
@@ -12,18 +12,14 @@
         <div class="container">
             <?php include_once 'menu.inc.php'; ?>
             <div class="hero-unit">
-                <iframe class="pull-right" src="https://embed.spotify.com/?uri=<?php echo constant("CORE_INVENT_SPOTIURI"); ?>" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>
+                <div style="float: right;"><iframe src="https://embed.spotify.com/?uri=<?php echo constant("CORE_INVENT_SPOTIURI"); ?>" width="300" height="150" frameborder="0" allowtransparency="true"></iframe></div>
                 <h1><?php echo constant("CORE_INVENT_NAME"); ?></h1>
                 <h1><small><?php echo constant("CORE_NAMEWITHOWNER"); ?></small></h3>
             </div>
-            <div class="container">
-                <div class="hero-unit">
-                    <h2><?php echo constant("CORE_INVENT_KEYWORDRANDOMIZER_NAME"); ?></h2>
-                    <a class="btn" href="javascript:void(0);" onClick="randomize();"><?php echo constant("CORE_INVENT_KEYWORDRANDOMIZER_ACTIONTEXT"); ?></a>
-                    <br />
-                    <br />
-                    <span class="badge badge-success"><h1 id="randomizer" class="well-large">&nbsp;...&nbsp;</h1></span>
-                </div>
+            <div class="hero-unit">
+                <div class="badge badge-success" style="float:right;"><h1 id="randomizer" class="well-large" style="text-align: right;">&nbsp;...&nbsp;</h1></div>
+                <h2><?php echo constant("CORE_INVENT_KEYWORDRANDOMIZER_NAME"); ?></h2>
+                <a class="btn" href="javascript:void(0);" onClick="randomize();"><?php echo constant("CORE_INVENT_KEYWORDRANDOMIZER_ACTIONTEXT"); ?></a>
             </div>
         </div>
     </body>
